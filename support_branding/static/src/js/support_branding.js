11f881bb7bb6f4e63bc8731095ab10c31d6da5da
zeroincombenze <info@shs-av.com>
2023-03-20 11:04:28 +0100

    [NEW] Initial setup 14.0

--- support_branding/static/src/js/support_branding.js
+++ support_branding/static/src/js/support_branding.js
@@ -1,170 +1,195 @@
-/* 
+/* Copyright 2012-2015 Therp
+ * Copyright 2016 - Tecnativa - Angel Moya <odoo@tecnativa.com>
+ * Copyright 2017 - redO2oo   - Robert Rottermann <robert@redO2oo.ch>
+ * Copyright 2018 - Therp BV
+ * Copyright 2021 - Sunflower IT
+ * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl). */
 
-   Copyright (C) 2012-2015 Therp BV
-   License: GNU AFFERO GENERAL PUBLIC LICENSE
-            Version 3 or any later version
+odoo.define("support_branding.CrashManager", function (require) {
+    "use strict";
+    var CrashManager = require("web.CrashManager").CrashManager;
+    var session = require("web.session");
+    var core = require("web.core");
+    var Wysiwyg = require("web_editor.wysiwyg.root");
 
-   Usage: if you run an OpenERP support company and you support
-   customers without an OPW, you can brand the OpenERP instance
-   accordingly using this module. Please enter the two variables
-   in the code below, 'support_name' and 'support_link'. They will
-   replace the unfriendly message about the OpenERP instance not
-   being supported.
-           
- */
-   
-openerp.support_branding = function(instance) {
-    var QWeb = instance.web.qweb,
-    _t = instance.web._t;
+    var _t = core._t;
 
-    instance.web.Session.include({
-        session_authenticate: function()
-        {
+    CrashManager.include({
+        init: function () {
             var self = this;
-            return this._super.apply(this, arguments)
-            .then(function()
-            {
-                return self.setup_branding()
-            });
-        },
-        session_init: function()
-        {
-            var self = this;
-            return this._super.apply(this, arguments)
-            .then(function()
-            {
-                return self.setup_branding()
-            });
-        },
-        setup_branding: function()
-        {
-            var self = this,
-                ir_config_parameter = new instance.web.Model('ir.config_parameter');
-            if(!self.uid)
-            {
-                return jQuery.when();
-            };
-            return (new instance.web.Model('ir.config_parameter'))
-                .query(['key', 'value'])
-                .filter([
-                    [
-                        'key', 'in', [
-                            'support_branding.support_email',
-                            'support_branding.company_name',
-                            'support_branding.company_color',
-                            'support_branding.company_url',
-                            'support_branding.release',
-                        ],
-                    ]
-                ])
-                .all()
-                .then(function(branding_values)
-                {
-                    _.each(branding_values, function(record)
-                    {
-                        self[record.key.replace('.', '_')] = record.value;
-                    });
+            $.when(this._super.apply(this, arguments)).then(function () {
+                self._rpc({
+                    model: "ir.config_parameter",
+                    method: "get_param",
+                    args: ["support_company"],
+                }).then(function (name) {
+                    self.support_cp_name = name;
                 });
-        },
-    });
 
-    instance.web.CrashManager.include({
-        show_error: function(error)
-        {
-            this._super.apply(this, arguments);
-            jQuery('.support-branding-submit-form').each(function()
-            {
-                var $form = jQuery(this),
-                    $button = $form.find('button'),
-                    $description = $form.find('textarea[name="description"]'),
-                    $subject = $form.find('input[name="subject"]'),
-                    $body = $form.find('input[name="body"]');
-                if(instance.client.session.support_branding_support_email)
-                {
-                    $form.attr(
-                        'action',
-                        'mailto:' + instance.client.session.support_branding_support_email);
-                    $form.parents('.ui-dialog').find('.ui-dialog-content')
-                        .css('max-height', '70vh');
-                    $button.click(function(ev)
-                    {
-                        var mail_mail = new instance.web.Model('mail.mail');
-                        if(!$description.val())
-                        {
-                            $description.parent().addClass('oe_form_invalid');
+                self._rpc({
+                    model: "ir.config_parameter",
+                    method: "get_param",
+                    args: ["support_company_url"],
+                }).then(function (url) {
+                    self.support_cp_url = url;
+                });
+
+                self._rpc({
+                    model: "ir.config_parameter",
+                    method: "get_param",
+                    args: ["support_email"],
+                }).then(function (email) {
+                    self.support_cp_email = email;
+                });
+
+                self._rpc({
+                    model: "ir.config_parameter",
+                    method: "get_param",
+                    args: ["support_release"],
+                }).then(function (release) {
+                    self.support_cp_release = release;
+                });
+
+                self._rpc({
+                    model: "ir.config_parameter",
+                    method: "get_param",
+                    args: ["support_branding_color"],
+                }).then(function (color) {
+                    self.support_cp_color = color;
+                });
+            });
+        },
+        show_error: function (error) {
+            var self = this;
+            this.wysiwyg = new Wysiwyg(self, {});
+            var subject =
+                session.username +
+                "@" +
+                session.db +
+                "[" +
+                session.server +
+                "]:" +
+                error.message;
+            var body = error.data.debug;
+            var inputs =
+                "" +
+                '<input type="hidden" name="subject" autocomplete="off" ' +
+                "value=" +
+                subject +
+                "/>\n" +
+                '<input type="hidden" class="sp-body" name="body" ' +
+                'autocomplete="off" value=\'' +
+                body +
+                "'/>";
+            return this._super.apply(this, arguments).opened(function () {
+                var $form = $(".support-branding-submit-form");
+                var $statement = $(".support-statement");
+                var $description = $(".support-desc");
+                var $button = $(".support-btn");
+                var $use_html_ck = $("#use_html_checker");
+                var $close_btn = $(".close");
+                var $body = $(".sp-body");
+                var $header = $form.parents(".modal-dialog").find(".modal-header");
+                var $footer = $form.parents(".modal-dialog").find(".modal-footer");
+                // Self.wysiwyg.attachTo($description);
+
+                $statement.prepend(inputs);
+                if (self.support_cp_email) {
+                    if (self.support_cp_name) {
+                        var title = "Support by " + self.support_cp_name;
+                        $('<h3 class="text-primary">' + title + "</h3>").insertBefore(
+                            ".support-branding-submit-form"
+                        );
+                        $button.text(
+                            _.str.sprintf(_t("Email to %s"), self.support_cp_name)
+                        );
+                    }
+                    $form
+                        .parents(".modal")
+                        .find(".modal-body")
+                        .css("max-height", "70vh");
+                    $use_html_ck.on("change", function () {
+                        if (this.checked) {
+                            if (self.wysiwyg.isDestroyed()) {
+                                self.wysiwyg = new Wysiwyg(self, {});
+                                self.wysiwyg.attachTo($description);
+                            } else self.wysiwyg.attachTo($description);
+                        } else {
+                            self.wysiwyg.destroy();
+                            $(".support-statement").append($description);
+                            if ($description.is(":hidden"))
+                                $description.css({display: "block"});
+                        }
+                    });
+
+                    $button.on("click", function (ev) {
+                        var $btn = $(this);
+                        if (!$description.val()) {
+                            $description.parent().addClass("oe_form_invalid");
                             ev.preventDefault();
                             return;
                         }
-                        mail_mail.call(
-                            'create',
-                            [{
-                                state: 'outgoing',
-                                auto_delete: false,
-                                email_to: instance.client.session.support_branding_support_email,
-                                subject: $subject.val(),
-                                body_html: jQuery('<div/>').append(
-                                    jQuery('<div/>').text($description.val()),
-                                    jQuery('<pre/>').text($body.val())
-                                ).html(),
-                            }])
-                        .then(function(mail_id)
-                        {
-                            return mail_mail.call('send', [[mail_id]]);
-                        }, function()
-                        {
-                            // if the call failed, fire the mailto link
-                            // hoping there is a properly configured email
-                            // client
-                            $body.val($description.val() + '\n' + $body.val())
-                            $button.unbind('click');
-                            $button.click();
-                        })
-                        .then(function()
-                        {
-                            $form.parents('.ui-dialog').dialog().dialog("close");
-                            $('.ui-widget-overlay').hide()
-                        });
                         ev.preventDefault();
+                        var error_code = "";
+                        var body_html = "";
+                        var desc =
+                            self.wysiwyg.$editor && self.wysiwyg.$editor.length
+                                ? self.wysiwyg.getValue()
+                                : $description.val();
+                        desc = `<div>${desc}</div>`;
+                        error_code = `<pre>${body}</pre>`;
+                        body_html = `<div>${desc}<br/>${error_code}</div>`;
+                        var params = {
+                            state: "outgoing",
+                            auto_delete: true,
+                            email_to: self.support_cp_email,
+                            subject: subject,
+                            body_html: body_html,
+                        };
+                        self._rpc({
+                            model: "mail.mail",
+                            method: "create",
+                            args: [params],
+                        }).then(
+                            function (mail_id) {
+                                if (mail_id) {
+                                    self._rpc({
+                                        model: "mail.mail",
+                                        method: "send",
+                                        args: [mail_id],
+                                    }).then(function () {
+                                        self.do_notify(
+                                            "Success",
+                                            "Support mail created!"
+                                        );
+                                        $close_btn.click();
+                                    });
+                                }
+                            },
+                            function () {
+                                $body.val(desc + "\n\n" + $body.val());
+                                $btn.unbind("click");
+                                $btn.click();
+                            }
+                        );
                     });
+                } else {
+                    $description.css({display: "none"});
+                    $button.css({display: "none"});
                 }
-                else
-                {
-                    $description.hide();
-                    $button.hide();
-                }
-                if(instance.client.session.support_branding_company_name)
-                {
-                    $button.text(
-                        _.str.sprintf(
-                            instance.web._t('Email to %s'),
-                            instance.client.session.support_branding_company_name));
-                }
-                $form.prependTo(
-                    $form.parents('.ui-dialog').find('.ui-dialog-buttonpane'));
-            });
-        }
-    });
+                $form.prependTo($footer);
+                // Hide "Ok" button since we have close on the dialog top
+                // Allow send email btn to close once done.
+                $footer.find("button:eq(1)").css({display: "none"});
 
-    instance.web.WebClient.include({
-        show_application: function()
-        {
-            var self = this;
-            return jQuery.when(this._super(this, arguments))
-            .then(function()
-            {
-                var $link = self.$el.find('.support_branding_link');
-                var text = instance.client.session.support_branding_company_name
-                $link.text(instance.client.session.support_branding_company_name);
-                $link.css('color', instance.client.session.support_branding_company_color);
-                $link.attr('href', instance.client.session.support_branding_company_url);
-                if(instance.client.session.support_branding_release)
-                {
-                    var $release = self.$el.find('.support_branding_release');
-                    $release.text(
-                        instance.web._t("Version") + " "
-                        + instance.client.session.support_branding_release);
+                if (self.support_cp_color) {
+                    $header.css({background: self.support_cp_color});
+                    $footer.css({background: self.support_cp_color});
+                } else {
+                    $header.css({background: ""});
+                    $footer.css({background: ""});
                 }
             });
         },
     });
-};
+});
